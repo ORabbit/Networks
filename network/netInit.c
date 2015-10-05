57282cc3cfeb99b392d1b6058833708cf5049c13
Omokolade Viyon Hunpatin <ohunpati@telos.mscs.mu.edu>
2015-10-04 20:25:32 -0500

    fixed netinit packet sniffer so now xinu can get its ip address on start up

--- network/netInit.c
+++ network/netInit.c
@@ -16,24 +16,22 @@ uchar* myipaddr;
 void netInit(void)
 {
 	uchar * packet[PKTSZ];
-	void *pkt = malloc(ETHER_SIZE + sizeof(struct arpPkt));
+	struct ethergram *eg = (struct ethergram*) packet;
+	bzero(packet,PKTSZ);
+	myipaddr = malloc(IPv4_ADDR_LEN);	
 	uchar *mac = malloc(ETH_ADDR_LEN);
-	struct arpPkt *temp2;
-	uchar *temp3=malloc(4);
 
 	open(ETH0);
 	arpDaemonId = create((void *)arpDaemon, INITSTK, 3, "ARP_DAEMON", 0);
 	control(ETH0, ETH_CTRL_GET_MAC, (long) mac, 0);
 	do {
-		read(ETH0, pkt, sizeof(pkt));
-		struct ethergram *temp = (struct ethergram *) pkt;
-		char *tempData = temp->data;
-		temp2 = (struct arpPkt *) tempData;
-		memcpy(temp3, &temp2->addrs[ARP_ADDR_DHA], IPv4_ADDR_LEN);
-		printf("PktType:%08x\tMAC:%02x:%02x:%02x:%02x:%02x:%u\r\nOURMAC:%02x:%02x:%02x:%02x:%02x:%02x\r\n", ntohs(temp2->prtype), temp2->addrs[ARP_ADDR_DHA], temp2->addrs[ARP_ADDR_DHA+1], temp2->addrs[ARP_ADDR_DHA+2], temp2->addrs[ARP_ADDR_DHA+3], temp2->addrs[ARP_ADDR_DHA+4], temp2->addrs[ARP_ADDR_DHA+5], mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
-	} while(memcmp(&temp2->addrs[ARP_ADDR_DHA], mac, sizeof(mac)) != 0);
+		read(ETH0, packet, PKTSZ);
+
+		printf("PktType:%08x\tMAC:%02x:%02x:%02x:%02x:%02x:%u\r\nOURMAC:%02x:%02x:%02x:%02x:%02x:%02x\r\n", ntohs(eg->type), eg->dst[0], eg->dst[1], eg->dst[2], eg->dst[3], eg->dst[4], eg->dst[5], mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
+	} while(memcmp(eg->dst, mac, sizeof(mac)) != 0);
 	//we found the packet with our ip address. Let's get out ip address
-	memcpy(myipaddr, &temp2->addrs[ARP_ADDR_DPA], IPv4_ADDR_LEN);
+	struct arpPkt* tmpArpPkt = (struct arpPkt*) &eg->data[0];
+	memcpy(myipaddr, &tmpArpPkt->addrs[ARP_ADDR_DPA], IPv4_ADDR_LEN);
 	printf("IP Address is: %u.%u.%u.%u\r\n", myipaddr[0], myipaddr[1], myipaddr[2], myipaddr[3]);
 	ready(arpDaemonId, 1); /* Starts an ARP Daemon for the backend. */
 	return;
