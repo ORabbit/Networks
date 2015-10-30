743aab3a66e0c759b84ce3047485e6e5d0a45e20
Casey O <lionspride4@gmail.com>
2015-10-30 16:26:18 -0500

    Added a icmpDaemon to icmp.c which handles all icmp packets that come in

---
+++ network/ip/icmp.c
@@ -0,0 +1,77 @@
+
+#include <xinu.h>
+#include <icmp.h>
+#include <arp.h>
+
+uchar packet[PKTSZ];
+
+void icmpDaemon(void)
+{
+	struct ethergram *eg;
+	struct icmpPkt *icmpPkt;
+	struct ipgram *ipPkt;
+	uchar *myMac = malloc(ETH_ADDR_LEN);
+	control(ETH0, ETH_CTRL_GET_MAC, (long)myMac, 0);
+
+	while (1)
+	{
+		//printf("icmpDaemon started loop\n");
+		bzero(packet, PKTSZ);
+		read(ETH0, packet, PKTSZ);
+//		printPacket(packet);
+		eg = (struct ethergram *)packet;
+		if (memcmp(eg->dst, myMac, sizeof(myMac)) != 0) /* Not our packet, drop */
+			continue;
+		//printf("icmpDaemon We received a packet for our mac\n");
+		icmpPkt = (struct icmpPkt *)&eg->data[0];
+		//printf("eg->type=%d arpPkt->hwtype=%d arpPkt->prtype=%08x\n",ntohs(eg->type), ntohs(arpPkt->hwtype), ntohs(arpPkt->prtype));
+		if (ntohs(eg->type)!=ETYPE_IPv4 || ntohs(icmpPkt->code) != 0
+			 || htons(icmpPkt->checksum) != checksum(icmpPkt, sizeof(struct icmpPkt*)))
+			continue;
+		//printf("icmpDaemon Got past first check\n");
+		ipPkt = (struct ipgram *)&icmpPkt->data[0];
+		if (htons(ipPkt->ver_ihl) != (IPv4_VERSION + IPv4_MIN_IHL) || ipPkt->tos != 0
+			|| htons(ipPkt->len) != IPv4_HDR_LEN || ipPkt->id != 0
+			|| ipPkt->flags_froff != 0 || htons(ipPkt->ttl) < 0 
+			|| htons(ipPkt->proto) != IPv4_PROTO_ICMP 
+			|| memcmp(ipPkt->dst, myipaddr, IP_ADDR_LEN) 
+			|| htons(ipPkt->chksum) != checksum(ipPkt, sizeof(ipPkt)))
+			continue;
+
+		if (memcmp(myipaddr, &ipPkt->addrs[ARP_ADDR_DPA], sizeof(myipaddr)) != 0)
+			continue;
+		//printf("icmpDaemon We received a packet for our mac and ip\n");
+
+		if (ntohs(icmpPkt->type) == ECHO_REQUEST) /* ECHO Request */
+		{
+			//printf("icmpDaemon ECHO Request recevied\n");
+			memcpy(eg->dst, eg->src, sizeof(eg->src));
+			memcpy(eg->src, myMac, sizeof(myMac));
+			eg->type = htons(ETYPE_IPv4);
+
+			icmpPkt->type = htons(ECHO_REPLY);
+			icmpPkt->code = 0;
+			icmpPkt->checksum = htons(checksum(icmpPkt, sizeof(struct icmpPkt*)));
+
+			// Almost entire ipPkt is already set up. Just changing src, dst, and checksum
+			memcpy(&ipPkt->dst, &ipPkt->src, IPv4_ADDR_LEN);
+			memcpy(&ipPkt->src, myipaddr, IPv4_ADDR_LEN);
+			ipPkt->chksum = htons(checksum(ipPkt, sizeof(ipPkt)));
+
+			write(ETH0, packet,ETHER_SIZE+ETHER_MINPAYLOAD);//(uchar*)((struct arpPkt *)((struct ethergram *)packet)->data)-packet);
+		}else if (ntohs(icmpPkt->type) == ECHO_REPLY) /* ECHO Reply */
+		{
+			uchar *data = malloc(sizeof(uchar) * 3);
+			data[0] = sizeof(packet);
+			data[1] = htons(ipPkt->ttl);
+			data[2] = (uchar)CLKTICKS_PER_SEC*10;
+			send((int)(long)data, recvtime(1));
+			recvtime(1);
+			free(data);
+		}else /* Some other op type, drop */
+			continue;
+	}
+	free(packet);
+}
+
+
